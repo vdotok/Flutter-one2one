d92ac8086a4328a8d56278b6091d5565acfd32ee
488taimoor <488taimoor@gmail.com>
2023-04-27 15:02:49 +0500

    config file updated

--- lib/src/core/config/config.dart
+++ lib/src/core/config/config.dart
@@ -1,3 +1,4 @@
-var URL = "https://q-tenant.vdotok.dev";
+// var URL = "https://q-tenant.vdotok.dev";
+var URL = "https://tenant.vdotok.com";
 const version = "/API/v0/";
-const String project_id = "1RN1RP";
+const String project_id = "808GIUIK";
